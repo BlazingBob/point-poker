7a90c133fd1cd60a9bd228bb2599c3a6ae2076dd
Jip Teeuwen <jipteeuwen@hotmail.com>
2024-08-09 16:41:36 +0200

    add show actions sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,14 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
+  def show
+    @session = Session.find(params[:id])
+  end
+
   def create
     @session = Session.new
     if @session.save!
-      redirect_to root_path, notice: 'Session created successfully'
+      redirect_to session_path(@session), notice: 'Session created successfully'
     else
       render :home, alert: 'Failed to create session'
     end
