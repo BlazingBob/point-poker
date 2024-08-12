ed62d946efffc7a2bdd094d6aad22260b954641c
Jip Teeuwen <jipteeuwen@hotmail.com>
2024-08-12 15:00:46 +0200

    get session and permit session id param from form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@
 
 class UsersController < ApplicationController
   def create
+    @session = Session.find(params[:session_id])
     @user = @session.users.new(user_params) # Zorg dat de user aan de session wordt gekoppeld
     if @user.save
       redirect_to session_path(@session), notice: 'User successfully created'
@@ -10,5 +11,9 @@ class UsersController < ApplicationController
     end
   end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
   end
 end
